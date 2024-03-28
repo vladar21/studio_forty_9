95e48b7cd6e7651b58bd10bcd1006c050d958f12
vladar21 <vladar21@gmail.com>
2024-03-28 01:17:30 +0000

    add setters and getters to Stock entity

--- src/InventoryBundle/Entity/Stock.php
+++ src/InventoryBundle/Entity/Stock.php
@@ -1,11 +1,78 @@
 <?php
 namespace App\InventoryBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="stocks")
+ */
 class Stock
 {
-    private $sku;
-    private $branch;
-    private $stock;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $sku;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $branch;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private float $stock;
+
+    // Getters and setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getBranch(): string
+    {
+        return $this->branch;
+    }
+
+    public function setBranch(string $branch): self
+    {
+        $this->branch = $branch;
+
+        return $this;
+    }
+
+    public function getStock(): float
+    {
+        return $this->stock;
+    }
+
+    public function setStock(float $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
 
 
 }
